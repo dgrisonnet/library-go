bdda9eaf498c04abd9143282b9ff5b9818ff55fe
Flavian Missi <fmissi@redhat.com>
2025-10-03 15:25:47 +0200

    plugin_controller: inject some more dependencies and add nil check

--- pkg/operator/encryption/controllers/plugin_controller.go
+++ pkg/operator/encryption/controllers/plugin_controller.go
@@ -6,6 +6,8 @@ import (
 
 	configv1 "github.com/openshift/api/config/v1"
 	operatorv1 "github.com/openshift/api/operator/v1"
+	configv1client "github.com/openshift/client-go/config/clientset/versioned/typed/config/v1"
+	configv1informers "github.com/openshift/client-go/config/informers/externalversions/config/v1"
 	applyoperatorv1 "github.com/openshift/client-go/operator/applyconfigurations/operator/v1"
 	"github.com/openshift/library-go/pkg/controller/factory"
 	"github.com/openshift/library-go/pkg/operator/encryption/statemachine"
@@ -17,10 +19,12 @@ import (
 
 type pluginController struct {
 	instanceName           string
+	targetNamespace        string
 	controllerInstanceName string
 
-	operatorClient operatorv1helpers.OperatorClient
-	secretClient   corev1client.SecretsGetter
+	operatorClient  operatorv1helpers.OperatorClient
+	secretClient    corev1client.SecretsGetter
+	apiServerClient configv1client.APIServerInterface
 
 	deployer                 statemachine.Deployer
 	provider                 Provider
@@ -31,18 +35,24 @@ type pluginController struct {
 
 func NewPluginController(
 	instanceName string,
+	targetNamespace string,
 	provider Provider,
 	deployer statemachine.Deployer,
 	preconditionsFulfilledFn preconditionsFulfilled,
+	apiServerClient configv1client.APIServerInterface,
 	operatorClient operatorv1helpers.OperatorClient,
 	secretClient corev1client.SecretsGetter,
 	encryptionSecretSelector metav1.ListOptions,
+	apiServerInformer configv1informers.APIServerInformer,
 	eventRecorder events.Recorder,
 ) factory.Controller {
 	c := pluginController{
 		instanceName:           instanceName,
 		controllerInstanceName: factory.ControllerInstanceName(instanceName, "KMSPlugin"),
+		targetNamespace:        targetNamespace,
 		operatorClient:         operatorClient,
+		apiServerClient:        apiServerClient,
+		provider:               provider,
 
 		preconditionsFulfilledFn: preconditionsFulfilledFn,
 	}
@@ -52,6 +62,8 @@ func NewPluginController(
 		WithControllerInstanceName(c.controllerInstanceName).
 		ResyncEvery(time.Minute).
 		WithInformers(
+			apiServerInformer.Informer(),
+			operatorClient.Informer(),
 			deployer,
 		).ToController(
 		c.controllerInstanceName,
@@ -62,7 +74,7 @@ func NewPluginController(
 func (c *pluginController) sync(ctx context.Context, syncCtx factory.SyncContext) (err error) {
 	// The status for this condition is intentionally omitted to ensure it's correctly set in each branch
 	degradedCondition := applyoperatorv1.OperatorCondition().
-		WithType("EncryptionStateControllerDegraded")
+		WithType("EncryptionKMSPluginControllerDegraded")
 
 	defer func() {
 		if degradedCondition == nil {
@@ -82,6 +94,15 @@ func (c *pluginController) sync(ctx context.Context, syncCtx factory.SyncContext
 				WithStatus(operatorv1.ConditionFalse)
 		}
 		return err // we will get re-kicked when the operator status updates
+	}
+
+	apiServer, err := c.apiServerClient.Get(ctx, "cluster", metav1.GetOptions{})
+	if err != nil {
+		return err
+	}
+	kmsConfig := apiServer.Spec.Encryption.KMS
+	if kmsConfig == nil {
+		return nil
 	}
 
 	switch c.kmsConfig.Type {
